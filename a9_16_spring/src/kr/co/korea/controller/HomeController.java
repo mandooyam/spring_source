69065fba6a73c507990b0c3d87ca36aebdfb3fb8
mandoo <codingmandoo@gmail.com>
2020-09-16 09:51:32 +0900

    HomeController에 @RequestMapping 추가

--- a9_16_spring/src/kr/co/korea/controller/HomeController.java
+++ a9_16_spring/src/kr/co/korea/controller/HomeController.java
@@ -3,6 +3,8 @@ package kr.co.korea.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -12,5 +14,13 @@ public class HomeController {
 		return "index";
 	}
 	
+	@RequestMapping(value="/board/write", method=RequestMethod.GET)
+	public String write() {
+		return "/board/write";
+	}
 	
+	@RequestMapping(value="/board", method=RequestMethod.GET)
+	public String list() {
+		return "/board/list";
+	}
 }
