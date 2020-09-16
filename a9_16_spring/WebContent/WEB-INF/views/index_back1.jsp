ea7b8269c5674fcf6884d92c2ca938e662f007a6
mandoo <codingmandoo@gmail.com>
2020-09-16 11:01:51 +0900

    <Controller의 RequestMapping에 파라메타 전달> TestController에 RequestMapping 등록

---
+++ a9_16_spring/WebContent/WEB-INF/views/index_back1.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>index</h1>
+<img src="images/1.png" alt="메이븐" /> 
+링크:<a href="test1">test1</a><br/>
+<a href="/a9_16_spring/test1">test1</a><br/>
+<a href="test2">test2</a><br/>
+<a href="board">board/list</a><br/>
+<a href="board/write">board/write</a><br/>
+<a href="test4">test4</a><br/>
+<a href="test6">test6</a><br/>
+<form action="test3" method="post">
+	<input type="submit" value="test3">
+</form>
+<form action="test5" method="post">
+	<input type="submit" value="test5">
+</form>
+<form action="test6" method="post">
+	<input type="submit" value="test6">
+</form>
+</body>
+</html>
--- a9_16_spring/src/kr/co/korea/controller/TestController.java
+++ a9_16_spring/src/kr/co/korea/controller/TestController.java
@@ -1,19 +1,112 @@
 package kr.co.korea.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class TestController {
-	
-	@RequestMapping(value="/test1", method=RequestMethod.GET)
-	public String test1() {
+
+	@RequestMapping(value = "/test1", method = RequestMethod.GET)
+	public String test1(HttpServletRequest request, HttpServletResponse response) {
+		// request: 값을 받아오는...
+
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String hobby[] = request.getParameterValues("hobby");
+		System.out.println(id);
+		System.out.println(name);
+		for (String str : hobby) {
+			System.out.println(str);
+		}
+
 		return "/test1";
 	}
 
-	@RequestMapping(value="/test2", method=RequestMethod.GET)
-	public String test2() {
+	@RequestMapping(value = "/test2", method = RequestMethod.POST)
+	public String test2(HttpServletRequest request, HttpServletResponse response) {
+
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String hobby[] = request.getParameterValues("hobby");
+		System.out.println(id);
+		System.out.println(name);
+		for (String str : hobby) {
+			System.out.println(str);
+		}
+
 		return "/test2";
 	}
+
+	@GetMapping("/test3")
+	public String test3(WebRequest request) { // WebRequest 클래스 통하여 주입
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String hobby[] = request.getParameterValues("hobby");
+		System.out.println("id:" + id);
+		System.out.println("name:" + name);
+		for (String str : hobby) {
+			System.out.println("hobby:" + str);
+		}
+		return "result";
+	}
+
+	@GetMapping("/test4/{id}/{name}/{hobby}")
+	public String test4(@PathVariable int id, @PathVariable int name, @PathVariable int hobby) {
+		System.out.println("id:" + id);
+		System.out.println("name:" + name);
+		System.out.println("hobby:" + hobby);
+		int add = id + name + hobby;
+		System.out.println("add:" + add);
+		return "result";
+	}
+
+	@GetMapping("/test5")
+	public String test5(@RequestParam int id, @RequestParam int name, @RequestParam int[] hobby) {
+		System.out.println("id:" + id);
+		System.out.println("name:" + name);
+		for (int str : hobby) {
+			System.out.println("hobby:" + str);
+		}
+		return "result";
+	}
+
+	@GetMapping("/test6")
+	public String test6(@RequestParam(value = "id") int value1, @RequestParam(value = "name") int value2,
+			@RequestParam(value = "hobby") int[] value3) {
+		System.out.println("id:" + value1);
+		System.out.println("name:" + value2);
+		for (int str : value3) {
+			System.out.println("hobby:" + str);
+		}
+		return "result";
+	}
+
+	@GetMapping("/test7")
+	public String test7(@RequestParam int id, @RequestParam(required = true) String name,
+			@RequestParam(defaultValue = "0") int hobby) {
+		System.out.println("id:" + id);
+		System.out.println("name:" + name);
+		System.out.println("hobby:" + hobby);
+		return "result";
+	}
+
+	@GetMapping("/test8")
+	public String test1(@RequestParam Map<String, String> map) {
+		String id = map.get("id");
+		String name = map.get("name");
+		System.out.printf("id :%s\n", id);
+		System.out.printf("name :%s\n", name);
+		return "result";
+	}
+
 }
