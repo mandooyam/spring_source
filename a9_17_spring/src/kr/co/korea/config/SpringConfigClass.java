ad129eb52bbb6f5459edfdebd18e6d5505e5c07d
mandoo <codingmandoo@gmail.com>
2020-09-17 09:33:57 +0900

    SpringConfigClass.java 수정(AbstractAnnotationConfigDispatcherServletInitializer 상속)

--- a9_17_spring/src/kr/co/korea/config/SpringConfigClass.java
+++ a9_17_spring/src/kr/co/korea/config/SpringConfigClass.java
@@ -1,5 +1,6 @@
 package kr.co.korea.config;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,35 +11,73 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class SpringConfigClass implements WebApplicationInitializer {
+//public class SpringConfigClass implements WebApplicationInitializer {
+//
+//	@Override
+//	public void onStartup(ServletContext servletContext) throws ServletException {
+//		// TODO Auto-generated method stub
+//		// Spring MVC 프로젝트 설정을 위해 작성하는 클래스의 객체를 생성한다. AnnotationConfigWebApplicationContext servletAppContext = new
+//		AnnotationConfigWebApplicationContext servletAppContext = new
+//				AnnotationConfigWebApplicationContext();
+//		
+//		servletAppContext.register(ServletAppContext.class);
+//		// 요청 발생 시 요청을 처리하는 서블릿을 DispatcherServlet으로 설정해준다
+//		
+//		DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
+//		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", dispatcherServlet);
+//		// 부가 설정
+//		servlet.setLoadOnStartup(1);
+//		servlet.addMapping("/");
+//		// Bean을 정의하는 클래스를 지정한다
+//		AnnotationConfigWebApplicationContext rootAppContext = new
+//		AnnotationConfigWebApplicationContext();
+//		rootAppContext.register(RootAppContext.class);
+//		ContextLoaderListener listener = new ContextLoaderListener(rootAppContext);
+//		servletContext.addListener(listener);
+//		// 파라미터 인코딩 설정
+//		FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter", CharacterEncodingFilter.class);
+//		filter.setInitParameter("encoding", "UTF-8");
+//		filter.addMappingForServletNames(null, false, "dispatcher");
+//		}
+//
+//
+//}
 
+
+public class SpringConfigClass extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	// 프로젝트에서 사용할 Bean들을 정의기 위한 클래스를 지정한다.
 	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
+	protected Class<?>[] getRootConfigClasses() {
 		// TODO Auto-generated method stub
-		// Spring MVC 프로젝트 설정을 위해 작성하는 클래스의 객체를 생성한다. AnnotationConfigWebApplicationContext servletAppContext = new
-		AnnotationConfigWebApplicationContext servletAppContext = new
-				AnnotationConfigWebApplicationContext();
-		
-		servletAppContext.register(ServletAppContext.class);
-		// 요청 발생 시 요청을 처리하는 서블릿을 DispatcherServlet으로 설정해준다
-		
-		DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
-		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", dispatcherServlet);
-		// 부가 설정
-		servlet.setLoadOnStartup(1);
-		servlet.addMapping("/");
-		// Bean을 정의하는 클래스를 지정한다
-		AnnotationConfigWebApplicationContext rootAppContext = new
-		AnnotationConfigWebApplicationContext();
-		rootAppContext.register(RootAppContext.class);
-		ContextLoaderListener listener = new ContextLoaderListener(rootAppContext);
-		servletContext.addListener(listener);
-		// 파라미터 인코딩 설정
-		FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter", CharacterEncodingFilter.class);
-		filter.setInitParameter("encoding", "UTF-8");
-		filter.addMappingForServletNames(null, false, "dispatcher");
-		}
+		return new Class[] {RootAppContext.class};
+	}
 
+	// Spring MVC 프로젝트 설정을 위한 클래스를 지정한다.
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {ServletAppContext.class};
+	}
 
+	// DispatcherServlet에 매핑할 요청 주소를 셋팅한다.
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+	// 파라미터 인코딩 필터 설정
+	@Override
+	protected Filter[] getServletFilters() {
+		// TODO Auto-generated method stub
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+		return new Filter[] {encodingFilter};
+
+	}
+	
+	
 }
