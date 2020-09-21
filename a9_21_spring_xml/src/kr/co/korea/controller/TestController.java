877300fd7e2e89cb1750afdef7040b068d44bd4f
mandoo <codingmandoo@gmail.com>
2020-09-21 12:52:37 +0900

    TestController에서 SqlSessionTemplate 로딩하여 호출하기(ing)

--- a9_21_spring_xml/src/kr/co/korea/controller/TestController.java
+++ a9_21_spring_xml/src/kr/co/korea/controller/TestController.java
@@ -1,10 +1,17 @@
 package kr.co.korea.controller;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kr.co.korea.beans.DataBean;
+
 @Controller
 public class TestController {
+	
+	@Autowired
+	SqlSessionTemplate sqltemp;
 	
 	@GetMapping("/test1")
 	public String test1() {
@@ -12,7 +19,8 @@ public class TestController {
 	}
 	
 	@GetMapping("/result")
-	public String result() {
+	public String result(DataBean dataBean) {
+		sqltemp.insert("test_db.insert_data", dataBean);
 		return "result";
 	}
 }
