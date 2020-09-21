66e7d7e3f65778047a618f83b170ca694e932b9a
mandoo <codingmandoo@gmail.com>
2020-09-21 10:18:44 +0900

    TestController2 생성, Mybatis 시작~

--- a9_21_spring/src/kr/co/korea/controller/TestController.java
+++ a9_21_spring/src/kr/co/korea/controller/TestController.java
@@ -3,7 +3,7 @@ package kr.co.korea.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+
 public class TestController {
 	
 //	@GetMapping("/test1")
