c8a3342dd765ae0cf647e459b59cf0a1067d9000
mandoo <codingmandoo@gmail.com>
2020-09-21 10:37:40 +0900

    memtable.sql, DataBean 생성

---
+++ a9_21_spring/WebContent/WEB-INF/memtable.sql
@@ -0,0 +1,7 @@
+drop table memtable;
+create table memtable(
+	id varchar2(100),
+	name varchar2(100),
+	pw varchar2(100)
+);
+select * from memtable;
---
+++ a9_21_spring/src/kr/co/korea/beans/DataBean.java
@@ -0,0 +1,28 @@
+package kr.co.korea.beans;
+
+public class DataBean {
+	
+	private String id;
+	private String name;
+	private String pw;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+}
