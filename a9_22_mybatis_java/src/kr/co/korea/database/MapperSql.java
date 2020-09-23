7d4d3bfdd79ae20d9ba4e9da83ae93a7292e4c7d
mandoo <codingmandoo@gmail.com>
2020-09-23 09:51:09 +0900

    과제1: MapperSql 작성

--- a9_22_mybatis_java/src/kr/co/korea/database/MapperSql.java
+++ a9_22_mybatis_java/src/kr/co/korea/database/MapperSql.java
@@ -2,8 +2,10 @@ package kr.co.korea.database;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import kr.co.korea.beans.DataBean;
 
@@ -17,4 +19,11 @@ public interface MapperSql {
 	
 	@Select("select count(*) from memtable")
 	int select_one();
+	
+	@Update("update memtable set pw=#{pw} where id=#{id}")
+	int update_data(DataBean dataBean);
+	
+	@Delete("delete from memtable where id=#{id} and pw=#{pw}")
+	int delete_data(DataBean dataBean);
+	
 }
