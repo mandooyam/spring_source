d4c8b6f9a7d14e7753d12da1f258bf065df4564b
mandoo <codingmandoo@gmail.com>
2020-09-24 11:08:38 +0900

    DataBean1,2,3,4 생성

---
+++ a9_24_scope_java/src/kr/co/korea/beans/DataBean3.java
@@ -0,0 +1,28 @@
+package kr.co.korea.beans;
+
+public class DataBean3 {
+
+	private String kor;
+	private String eng;
+	private String mat;
+	
+	public String getKor() {
+		return kor;
+	}
+	public void setKor(String kor) {
+		this.kor = kor;
+	}
+	public String getEng() {
+		return eng;
+	}
+	public void setEng(String eng) {
+		this.eng = eng;
+	}
+	public String getMat() {
+		return mat;
+	}
+	public void setMat(String mat) {
+		this.mat = mat;
+	}
+	
+}
