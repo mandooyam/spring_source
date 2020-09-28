b42a03e7ba7c7d2be1de3245519d51d726d60c6a
mandoo <codingmandoo@gmail.com>
2020-09-28 11:21:19 +0900

    DataBean클래스와 KeyValueBean클래스 생성

---
+++ a9_28_form/src/kr/co/korea/beans/DataBean.java
@@ -0,0 +1,119 @@
+package kr.co.korea.beans;
+
+public class DataBean {
+	
+	private String uid;
+	private String name;
+	private String pwd;
+	private String intro;
+	private String job;
+	private String job1;
+	private String job2;
+	private String job3;
+	private String hobby1[];
+	private String hobby2[];
+	private String hobby3[];
+	private String hobby4[];
+	private String gender1;
+	private String gender2;
+	private String gender3;
+	private String gender4;
+	
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getJob1() {
+		return job1;
+	}
+	public void setJob1(String job1) {
+		this.job1 = job1;
+	}
+	public String getJob2() {
+		return job2;
+	}
+	public void setJob2(String job2) {
+		this.job2 = job2;
+	}
+	public String getJob3() {
+		return job3;
+	}
+	public void setJob3(String job3) {
+		this.job3 = job3;
+	}
+	public String[] getHobby1() {
+		return hobby1;
+	}
+	public void setHobby1(String[] hobby1) {
+		this.hobby1 = hobby1;
+	}
+	public String[] getHobby2() {
+		return hobby2;
+	}
+	public void setHobby2(String[] hobby2) {
+		this.hobby2 = hobby2;
+	}
+	public String[] getHobby3() {
+		return hobby3;
+	}
+	public void setHobby3(String[] hobby3) {
+		this.hobby3 = hobby3;
+	}
+	public String[] getHobby4() {
+		return hobby4;
+	}
+	public void setHobby4(String[] hobby4) {
+		this.hobby4 = hobby4;
+	}
+	public String getGender1() {
+		return gender1;
+	}
+	public void setGender1(String gender1) {
+		this.gender1 = gender1;
+	}
+	public String getGender2() {
+		return gender2;
+	}
+	public void setGender2(String gender2) {
+		this.gender2 = gender2;
+	}
+	public String getGender3() {
+		return gender3;
+	}
+	public void setGender3(String gender3) {
+		this.gender3 = gender3;
+	}
+	public String getGender4() {
+		return gender4;
+	}
+	public void setGender4(String gender4) {
+		this.gender4 = gender4;
+	}
+	
+}
