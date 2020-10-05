55f683145788d6d33a291a55904ab6af99474769
mandoo <codingmandoo@gmail.com>
2020-10-05 10:20:07 +0900

    인터셉터 파일 작성

---
+++ a10_5_interceptor/src/kr/co/korea/interceptor/TestLoginInterceptor.java
@@ -0,0 +1,33 @@
+package kr.co.korea.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class TestLoginInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("--------------------------------------------");
+		System.out.println("(1)preHandle Login test");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("(3)postHandle Login test");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("(5)afterCompletion Login test");
+		System.out.println("---------------------------------");
+	}
+
+	
+}
