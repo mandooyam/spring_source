b727e7f3bdfa80885d74c9da6c5bfdf5c1e60721
mandoo <codingmandoo@gmail.com>
2020-10-05 11:55:48 +0900

    DataBean 생성 & 작성

--- a10_5_interceptor/src/kr/co/korea/controller/TestController3.java
+++ a10_5_interceptor/src/kr/co/korea/controller/TestController3.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.korea.beans.DataBean;
+
 @RestController
 public class TestController3 {
 	// (1)
@@ -17,17 +19,17 @@ public class TestController3 {
 
 	// (2) 자바의 객체나 리스트 등을 JSON 문서로 구성하여 값 자체를 브라우저로 전달
 	// (2) pom.xml 에서 jackson-databind 라이브러리 이용하여 데이터 전달가능
-//	@GetMapping("/test4")
-//	public ResponseEntity<ArrayList<DataBean>> test4() {
-//		DataBean bean1 = new DataBean("111", "111", "홍길동");
-//		DataBean bean2 = new DataBean("222", "222", "이기자");
-//		DataBean bean3 = new DataBean("333", "333", "최고야");
-//		ArrayList<DataBean> list = new ArrayList<DataBean>();
-//		list.add(bean1);
-//		list.add(bean2);
-//		list.add(bean3);
-//		ResponseEntity<ArrayList<DataBean>> entry = new ResponseEntity<ArrayList<DataBean>>(list, HttpStatus.OK);
-//		return entry;
-//	}
+	@GetMapping("/test4")
+	public ResponseEntity<ArrayList<DataBean>> test4() {
+		DataBean bean1 = new DataBean("111", "111", "홍길동");
+		DataBean bean2 = new DataBean("222", "222", "이기자");
+		DataBean bean3 = new DataBean("333", "333", "최고야");
+		ArrayList<DataBean> list = new ArrayList<DataBean>();
+		list.add(bean1);
+		list.add(bean2);
+		list.add(bean3);
+		ResponseEntity<ArrayList<DataBean>> entry = new ResponseEntity<ArrayList<DataBean>>(list, HttpStatus.OK);
+		return entry;
+	}
 
 }
