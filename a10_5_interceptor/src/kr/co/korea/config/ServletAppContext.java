f726750477327a62a94cb94867078e55664515fe
mandoo <codingmandoo@gmail.com>
2020-10-05 10:24:20 +0900

    ServletAppContext에 등록

--- a10_5_interceptor/src/kr/co/korea/config/ServletAppContext.java
+++ a10_5_interceptor/src/kr/co/korea/config/ServletAppContext.java
@@ -3,9 +3,14 @@ package kr.co.korea.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.co.korea.interceptor.TestLoginInterceptor;
+import kr.co.korea.interceptor.TestWriterInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -26,5 +31,21 @@ public class ServletAppContext implements WebMvcConfigurer{
 		registry.jsp("/WEB-INF/views/", ".jsp");
 	}
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addInterceptors(registry);
+		
+		TestLoginInterceptor testLoginInterceptor = new TestLoginInterceptor();
+		InterceptorRegistration reg1 = registry.addInterceptor(testLoginInterceptor);
+		reg1.addPathPatterns("/user/modify", "/user/logout", "/board/*");
+		reg1.excludePathPatterns("/board/main");
+		// reg1.addPathPatterns("/**");
+		
+		TestWriterInterceptor testWriterInterceptor = new TestWriterInterceptor();
+		InterceptorRegistration reg2 = registry.addInterceptor(testWriterInterceptor);
+		reg2.addPathPatterns("/board/modify", "/board/delete");
+		// board/modify 수행시 2회 수행됨. reg1에 "board/*" 등록되어 있음
+	}
 	
 }
