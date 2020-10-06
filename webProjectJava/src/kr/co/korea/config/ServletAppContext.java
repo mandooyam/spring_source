f71d52f05abf200ad36ab0a3bf367347c9a3ebd6
mandoo <codingmandoo@gmail.com>
2020-10-06 11:29:35 +0900

    3) ServletAppContext.java에 등록

--- webProjectJava/src/kr/co/korea/config/ServletAppContext.java
+++ webProjectJava/src/kr/co/korea/config/ServletAppContext.java
@@ -1,15 +1,26 @@
 package kr.co.korea.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import kr.co.korea.mapper.BoardMapper;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("kr.co.korea.controller")
+//1) 
+@PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer{
 
 	@Override
@@ -26,5 +37,44 @@ public class ServletAppContext implements WebMvcConfigurer{
 		registry.jsp("/WEB-INF/views/", ".jsp");
 	}
 
+	// 2)
+	@Value("${db.classname}")
+	private String db_classname;
+	@Value("${db.url}")
+	private String db_url;
+	@Value("${db.username}")
+	private String db_username;
+	@Value("${db.password}")
+	private String db_password;
+
+	// 3) 데이터베이스 접속 정보 관리 (org.apache.commons.dbcp2.BasicDataSource)
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName(db_classname);
+		source.setUrl(db_url);
+		source.setUsername(db_username);
+		source.setPassword(db_password);
+		// System.out.println("db connect");
+		return source;
+	}
+
+	// 4) 쿼리문과 접속 관리하는 객체
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(source);
+		SqlSessionFactory factory = factoryBean.getObject();
+		// System.out.println("sql");
+		return factory;
+	}
+
+	// 5) 쿼리문 실행을 위한 객체 (BoardMapper생성할것)
+	@Bean
+	public MapperFactoryBean<BoardMapper> getBoardMapper(SqlSessionFactory factory) throws Exception {
+		MapperFactoryBean<BoardMapper> factoryBean = new MapperFactoryBean<BoardMapper>(BoardMapper.class);
+		factoryBean.setSqlSessionFactory(factory);
+		return factoryBean;
+	}
 	
 }
